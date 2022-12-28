2164b5a8e10dccefead70d91f275c048d277a90b
5Suminsss2 <sumin.kim.dev@gmail.com>
2022-12-28 14:05:24 +0900

    Feat: 카테고리 모달 취소버튼 추가 및 카테고리 추가 axios post 코드 작성

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 import styled from 'styled-components';
+import axios from 'axios';
 
 function AddCategory () {  
     let subtitle;
     const [modalIsOpen, setIsOpen] = useState(false);
+    const [category, setCategory] = useState("");
 
     function openModal() {
         setIsOpen(true);
@@ -15,27 +17,46 @@ function AddCategory () {
     }
 
     function closeModal() {
-        setIsOpen(false);
+      setIsOpen(false);
+    }
+
+    const onChange = (e) => {
+      const { value } = e.target;
+      setCategory(value);
+    }
+
+    const handleSubmit = async() => {
+      // 로그인 기능 구현 후 데이터 수정하기
+      await axios.post("/category",
+        {
+          categoryName: category,
+          reviewCount: 0,
+          userId: 1
+        }
+      )
     }
 
     return(
-        <div>
-            <div onClick={openModal}>+</div>
-            <Modal
-                isOpen={modalIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Example Modal"
-            >
-                <Title>Add Category</Title>
-                <Form>
-                    <Input placeholder='카테고리를 입력하세요'/>
-                    <Submitbutton onClick={closeModal}>등록</Submitbutton>
-                </Form>
-            </Modal>
-        </div>
-    )
+      <div>
+        <div onClick={openModal}>+</div>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <Title>Add Category</Title>
+          <Form>
+            <Input placeholder="카테고리를 입력하세요" onChange={onChange} value={category} />
+            <ButtonPart>
+              <Submitbutton onClick={handleSubmit}>등록</Submitbutton>
+              <Submitbutton onClick={closeModal}>취소</Submitbutton>
+            </ButtonPart>
+          </Form>
+        </Modal>
+      </div>
+    );
 }
 
 // CSS
@@ -72,10 +93,21 @@ const Input = styled.input`
   }
 `;
 
+// 버튼 
+const ButtonPart = styled.section`
+  display: flex;
+  justify-content: space-around;
+  width: 80%;
+`
+
 const Submitbutton = styled.button`
   width: 50px;
   margin-top: 15px;
   border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #e5e7eb;
+  }
 `;
 
 export default AddCategory;
